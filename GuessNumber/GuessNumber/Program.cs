7ab1194388d469ec647540363ea4c860c5b69eb2
Jack <46061424+JKingRolling@users.noreply.github.com>
2020-09-08 17:33:23 +0800

    guess number task 0908
    编写一个控制台程序
    程序抛出一个 0 ～ 99999 之间的随机整数，不告诉用户，让用户来猜
    用户每次输入一个整数，程序提示是猜大了还是小了
    用户多次尝试后，猜对了，游戏结束，并提示一共猜了多少次

---
+++ GuessNumber/GuessNumber/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GuessNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int randomNumber, guessNumber;
+            int times = 1;
+            //生成随机数
+            Random rnd = new Random();
+            randomNumber = rnd.Next(99999);
+
+            //输入猜的数
+            Console.WriteLine("Please guess this number:");
+            guessNumber = Convert.ToInt32(Console.ReadLine());
+            
+            while (guessNumber != randomNumber)
+            {
+                if (guessNumber < randomNumber)
+                {
+                    Console.WriteLine("Too small,guess again:");
+                    guessNumber = Convert.ToInt32(Console.ReadLine());
+                    times++;
+                }
+                else if (guessNumber > randomNumber)
+                {
+                    Console.WriteLine("Too big,guess again:");
+                    guessNumber = Convert.ToInt32(Console.ReadLine());
+                    times++;
+                }
+            }
+            Console.WriteLine("Guess Right!Your had guessed {0} times!",times);
+            Console.ReadKey();         
+        }
+    }
+}
