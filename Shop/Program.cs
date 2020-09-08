cdef85d31c6e4a40f23d2f1f49c86d759487c176
Jack <46061424+JKingRolling@users.noreply.github.com>
2020-09-08 17:31:00 +0800

    shop emulator
    模拟一个购物终端
    命令行提示一个物品列表
    用户输入物品编号进行购买
    购买后有提示，按任意键返回购买页面

---
+++ Shop/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] goods = { "apple", "banana", "orange"};
+            String[] prices = { "100", "200", "300"};
+            
+            while(true)
+            {
+                Console.WriteLine("欢迎来到水果店！");
+                int i = 0;
+                for (i = 0; i < goods.Length; i++)
+                {
+                    Console.WriteLine("{0}:{1}的价格是{2}元", i+1, goods[i], prices[i]);
+                }
+                Console.WriteLine("请输入宁要购买的商品的编号：");
+                String chooseStr;
+                int chooseNum;
+                //类型转换
+                chooseStr = Console.ReadLine();
+                chooseNum = Convert.ToInt32(chooseStr);
+
+                if(chooseNum == 0)
+                {
+                    Console.Clear();
+                    Console.WriteLine("你没钱啦，爪巴！");
+                    Console.ReadKey();
+                }
+                else if(chooseNum < 0 || chooseNum > 3)
+                {
+                    Console.WriteLine("输入编号有误，请瞪大眼睛重新输入！(按任意键继续购买)");
+                    Console.ReadKey();
+                }
+                else
+                {
+                    Console.WriteLine("你购买的商品是：{0} , 价格为：{1} (按任意键继续购买)", goods[chooseNum-1], prices[chooseNum-1]);
+                    Console.ReadKey();
+                }
+                
+                Console.Clear();
+
+            }
+        }
+    }
+}
